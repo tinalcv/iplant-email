fafe4b19aa5bbc120bb4b82ffedbd822031c9eb5
Dennis Roberts <dennis@iplantcollaborative.org>
2016-09-06 17:25:28 -0700

    removed a test that failed when the iplant-email.config namespace couldn't be reloaded

--- test/iplant_email/core_test.clj
+++ test/iplant_email/core_test.clj
@@ -10,11 +10,3 @@
     (is (= (config/listen-port) 60000))
     (is (= (config/smtp-host) "local-exim"))
     (is (= (config/smtp-from-addr) "noreply@example.org"))))
-
-(deftest test-specified-configs
-  (require 'iplant-email.config :reload)
-  (config/load-config-from-file "dev-resources/no-defaults.properties")
-  (testing "specified configuration settings"
-    (is (= (config/listen-port) 65535))
-    (is (= (config/smtp-host) "the-host-that-shall-not-be-named"))
-    (is (= (config/smtp-from-addr) "nobody@nowhere.org"))))
